3af5db24ac88a517dbb7e6cf0cd91db735df2529
Chris Weed <chris@cjweed.com>
2023-08-27 00:11:07 -0500

    Field displaying, starting to show labels

--- src/Field.tsx
+++ src/Field.tsx
@@ -1,22 +1,50 @@
-import { createEffect, type Component, createSignal } from 'solid-js'
+import { type Component, createSignal, For } from 'solid-js'
+import { throttle } from 'lodash-es'
 import styles from './Field.module.css'
+import Soccer from './assets/Soccer.svg'
+// import SoccerBuildout from './assets/Soccer-Buildout.svg'
+
+const labels = [
+  {
+    lineName: 'goalline',
+    id: 'goalline-midfield',
+    x: 52.5,
+    y: 24,
+    text: 'A',
+  },
+]
 
 export const Field: Component = () => {
-  const [width, setWidth] = createSignal<number>()
-  const [height, setHeight] = createSignal<number>()
-  // 1.48 ratio // 0.69565
+  const [fieldHeight, setFieldHeight] = createSignal(
+    document.getElementById('soccer-field')?.clientHeight,
+  )
 
-  createEffect(() => {
-    const parentWidth =
-      document.querySelector('#field-wrapper')?.clientWidth ?? 0
-    const parentHeight =
-      (document.querySelector('#field-wrapper')?.clientWidth ?? 0) * 0.69565
-
-    setWidth(parentWidth)
-    setHeight(parentHeight)
-  })
+  window.addEventListener(
+    'resize',
+    throttle(() => {
+      console.log('Re-size')
+      setFieldHeight(document.getElementById('soccer-field')?.clientHeight)
+    }, 200),
+  )
 
   return (
-    <canvas class={styles.Field} width={width()} height={height()} id="field" />
+    <div
+      class={styles.FieldWrapper}
+      style={{
+        height: `${fieldHeight()}px`,
+      }}
+    >
+      <Soccer id="soccer-field" style={{ width: '100%' }} />
+      <For each={labels}>
+        {(label) => (
+          <div
+            class={styles.Label}
+            style={{ left: `${label.x}%`, top: `${label.y}%` }}
+          >
+            {label.text}
+          </div>
+        )}
+      </For>
+    </div>
   )
 }
