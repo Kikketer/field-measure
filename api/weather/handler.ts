90d66b5a5c3d1bc4de2dcf5f37cd2834ce9259f2
Chris Weed <chris@cjweed.com>
2023-08-29 22:21:40 -0500

    Attempt vercel serverless

---
+++ api/weather/handler.ts
@@ -0,0 +1,9 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node'
+
+export default async (request: VercelRequest, response: VercelResponse) => {
+  response.status(200).json({
+    body: request.body,
+    query: request.query,
+    cookies: request.cookies,
+  })
+}
