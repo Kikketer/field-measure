30029de10d551671d2fc16cdc582c47a48fbc93f
Chris Weed <chris@cjweed.com>
2023-09-09 09:32:41 -0500

    Cleaned

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,11 @@
-import { Outlet, Route, Router, Routes } from '@solidjs/router'
-import { Component, createResource, createSignal } from 'solid-js'
+import { Route, Router, Routes } from '@solidjs/router'
+import { Component } from 'solid-js'
 import { AddField } from './AddField'
 import styles from './App.module.css'
+import { Authenticated } from './Authenticated'
 import { FieldDetail } from './FieldDetail'
 import { FieldList } from './FieldList'
-import { getFields } from './FieldStore'
 import { Login } from './Login'
-import { Authenticated } from './Authenticated'
 
 const App: Component = () => {
   return (
