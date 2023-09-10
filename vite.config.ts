b99f7d2dc15de3c910920664862b0e88e80d151c
Chris Weed <chris@cjweed.com>
2023-09-09 21:50:39 -0500

    PWA time

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
+import { VitePWA } from 'vite-plugin-pwa'
 // import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
@@ -10,6 +11,41 @@ export default defineConfig({
     */
     // devtools(),
     solidPlugin(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        theme_color: '#274801',
+        background_color: '#152601',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        name: 'Field Manager',
+        short_name: 'Field Manager',
+        description: 'Manage Youth Soccer FIelds',
+        icons: [
+          {
+            src: '/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-256x256.png',
+            sizes: '256x256',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-384x384.png',
+            sizes: '384x384',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
   ],
   server: {
     port: 3000,
