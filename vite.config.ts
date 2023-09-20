77c40344b5bfb3b9536ddfdde74ae531e7436420
Chris Weed <chris@cjweed.com>
2023-09-19 23:42:57 -0500

    Image update

--- vite.config.ts
+++ vite.config.ts
@@ -24,22 +24,17 @@ export default defineConfig({
         description: 'Manage Youth Soccer FIelds',
         icons: [
           {
-            src: '/icon-192x192.png',
+            src: '/192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/icon-256x256.png',
+            src: '/256.png',
             sizes: '256x256',
             type: 'image/png',
           },
           {
-            src: '/icon-384x384.png',
-            sizes: '384x384',
-            type: 'image/png',
-          },
-          {
-            src: '/icon-512x512.png',
+            src: '/512.png',
             sizes: '512x512',
             type: 'image/png',
           },
