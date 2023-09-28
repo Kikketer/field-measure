dfd37d83a60ebdd5c843698aead01054df338a3a
Chris Weed <chris@cjweed.com>
2023-09-28 08:10:41 -0500

    Tried again with the tests

--- src/pages/FieldList.test.tsx
+++ src/pages/FieldList.test.tsx
@@ -2,9 +2,8 @@ import { render, fireEvent } from '@solidjs/testing-library'
 import { FieldList } from './FieldList'
 
 describe('FieldList', () => {
-  it('should render', () => {
-    const { getByRole, unmount } = render(() => <FieldList />)
+  it('should be able to test components like a "real" framework', () => {
+    const { getByRole } = render(() => <FieldList />)
     expect(true).toBe(true)
-    unmount()
   })
 })
--- src/utilities/convertToFeet.test.ts
+++ src/utilities/convertToFeet.test.ts
@@ -4,4 +4,12 @@ describe('convertToFeet', () => {
   it('should convert 0.5 to 6"', () => {
     expect(convertToFeet(0.5)).toBe('0\' 6"')
   })
+
+  it('should convert 12.5 to 12\' 6"', () => {
+    expect(convertToFeet(12.5)).toBe('12\' 6"')
+  })
+
+  it("should convert 12.99 to 13'", () => {
+    expect(convertToFeet(12.99)).toBe("13'")
+  })
 })
