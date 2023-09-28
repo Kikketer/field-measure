bbba4014d3c7ab3d255b6806dcb6895a0dd880e0
Chris Weed <chris@cjweed.com>
2023-09-28 08:21:03 -0500

    Added version string

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,8 +1,14 @@
 import { Component, JSX } from 'solid-js'
 import styles from './Page.module.css'
+import { version } from '../../package.json'
 
 export const Page: Component<{
   children: JSX.Element
 }> = ({ children }) => {
-  return <div class={styles.Page}>{children}</div>
+  return (
+    <div class={styles.Page}>
+      {children}
+      <div class={styles.Footer}>s: {version}</div>
+    </div>
+  )
 }
