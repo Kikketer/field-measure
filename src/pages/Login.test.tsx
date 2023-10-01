db33941df21217c67454250c900a3268938ace83
Chris Weed <chris@cjweed.com>
2023-09-30 19:25:39 -0500

    Started calculations...

--- src/pages/Login.test.tsx
+++
@@ -1,9 +0,0 @@
-// import { render, fireEvent } from '@solidjs/testing-library'
-// import { Login } from './Login'
-
-// describe('Login', () => {
-//   it('should render', () => {
-//     render(() => <Login />)
-//     expect(true).toBe(true)
-//   })
-// })
