5d624135e17b76175785c066983e24ddcef4621d
Chris Weed <chris@cjweed.com>
2023-09-30 19:29:00 -0500

    Corrected failures of WebStorm

--- src/utilities/calculateConditions.ts
+++ src/utilities/calculateConditions.ts
@@ -1,8 +1,12 @@
-import { Field } from "./types";
+import { Field } from './types'
 
 export const predictNextPaintDate = () => {}
 
-export const adjustRainFactorAndDryDays = ({ currentField }: { currentField: Field}): Field => {
+export const adjustRainFactorAndDryDays = ({
+  currentField,
+}: {
+  currentField: Field
+}): Field => {
   /**
    * Max days with no rainfall = 14
    * If unplayable and rainfall has happened, adjust the rainfall factor to match the duration since last painting
