fdcf8d3c51cabb81983f2bdc893941ca2b955a1a
Chris Weed <chris@cjweed.com>
2023-10-03 18:58:54 -0500

    Adjusted a lint error

--- src/pages/FieldList.tsx
+++ src/pages/FieldList.tsx
@@ -24,23 +24,18 @@ import styles from './FieldList.module.css'
 
 const groupFields = (fields: Field[]): { [groupName: string]: Field[] } => {
   // Group by "group"
-  const groupedFields: { [groupName: string]: Field[] } = fields.reduce(
-    (acc: { [groupName: string]: Field[] }, field) => {
-      if (!field.group) {
-        acc.Other ? acc.Other.push(field) : (acc.Other = [field])
-        return acc
-      }
-
-      if (!acc[field.group]) {
-        acc[field.group] = []
-      }
-      acc[field.group].push(field)
+  return fields.reduce((acc: { [groupName: string]: Field[] }, field) => {
+    if (!field.group) {
+      acc.Other ? acc.Other.push(field) : (acc.Other = [field])
       return acc
-    },
-    {},
-  )
+    }
 
-  return groupedFields
+    if (!acc[field.group]) {
+      acc[field.group] = []
+    }
+    acc[field.group].push(field)
+    return acc
+  }, {})
 }
 
 export const FieldList: Component = () => {
