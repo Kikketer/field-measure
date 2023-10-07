c8a8899f1f0b44e0aa3ccf732eff92473ed527fc
Chris Weed <chris@cjweed.com>
2023-10-07 10:34:34 -0500

    Added the weather api key

--- supabase/functions/weather-check/index.ts
+++ supabase/functions/weather-check/index.ts
@@ -4,6 +4,7 @@ import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 console.log(`${new Date().toISOString()}: version: ${Deno.version.deno}`)
 
 Deno.serve(async (req: Request) => {
+  const weatherApiKey = Deno.env.get(WEATHER_API_KEY)
   try {
     // Create a Supabase client with the Auth context of the logged in user.
     const supabaseClient = createClient(
@@ -29,6 +30,8 @@ Deno.serve(async (req: Request) => {
       .from('fields')
       .select('*')
     if (error) throw error
+
+    // TODO Call the weather API to get this users location data
 
     return new Response(JSON.stringify({ user, fields }), {
       headers: { 'Content-Type': 'application/json' },
