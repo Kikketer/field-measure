63a7f41ada178b9903eb12ed9594a16b50af96cf
Chris Weed <chris@cjweed.com>
2023-10-13 20:07:06 -0500

    Weather fetching

---
+++ supabase/functions/weather-check/getWeather.ts
@@ -0,0 +1,42 @@
+export const getWeather = async ({ locationZip }: { locationZip: string }) => {
+  // 12.7 mm = .5 inches = 1 rain day
+  // So at least 12.7mm adds 1 to the rainfallDays
+  const weatherApiKey = Deno.env.get('WEATHER_API_KEY')
+
+  const locationResponse = await fetch(
+    `http://api.openweathermap.org/geo/1.0/zip?zip=${locationZip},US&appid=${weatherApiKey}`,
+    {
+      headers: {
+        accept: 'application/json',
+      },
+    },
+  )
+
+  const location = await locationResponse.json()
+
+  // Calling the weather api to get the rainfall for yesterday
+  // Fun thing is that with day 0 it uses the previous month, nice!
+  const yesterday = new Date(
+    new Date().getFullYear(),
+    new Date().getMonth(),
+    new Date().getDate() - 1,
+  )
+  const yesterdayString = `${yesterday.getFullYear()}-${
+    yesterday.getMonth() + 1
+  }-${String(yesterday.getDate()).padStart(2, '0')}`
+
+  console.log(
+    `https://api.openweathermap.org/data/3.0/onecall/day_summary?lat=${location.lat}5&lon=${location.lon}&date=${yesterdayString}`,
+  )
+
+  const weather = await fetch(
+    `https://api.openweathermap.org/data/3.0/onecall/day_summary?lat=${location.lat}5&lon=${location.lon}&date=${yesterdayString}&units=imperial&appid=${weatherApiKey}`,
+    {
+      headers: {
+        accept: 'application/json',
+      },
+    },
+  )
+
+  return await weather.json()
+}
--- supabase/functions/weather-check/index.ts
+++ supabase/functions/weather-check/index.ts
@@ -1,4 +1,5 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+import { getWeather } from './getWeather.ts'
 import { corsHeaders } from './_cors.ts'
 
 // Just signal that we were called
@@ -31,54 +32,25 @@ Deno.serve(async (req: Request) => {
       data: { user },
     } = await supabaseClient.auth.getUser()
 
-    // And we can run queries in the context of our authenticated user
+    console.log('Getting weather for user ', {
+      email: user?.email,
+      id: user?.id,
+    })
+
+    const weather = await getWeather({ locationZip: '53575' })
+
+    // If rainfall for the day is above 3, mark it as a rainfall day
     const { data: fields, error } = await supabaseClient
+      .update('')
       .from('fields')
       .select('*')
     if (error) throw error
-
-    // 12.7 mm = .5 inches = 1 rain day
-    // So at least 12.7mm adds 1 to the rainfallDays
-    const weatherApiKey = Deno.env.get('WEATHER_API_KEY')
-    // TODO Call the weather API to get this users location data
-    console.log(`Call weather: ${weatherApiKey.substring(0, 5)}`)
-
-    // Calling the weather api to get the rainfall for yesterday
-    // Fun thing is that with day 0 it uses the previous month, nice!
-    const yesterday = new Date(
-      new Date().getFullYear(),
-      new Date().getMonth(),
-      new Date().getDate() - 1,
-    )
-    const yesterdayString = `${yesterday.getFullYear()}-${
-      yesterday.getMonth() + 1
-    }-${String(yesterday.getDate()).padStart(2, '0')}`
-
-    // These will be pulled from the paintteam table eventually:
-    const lat = 42.9295
-    const long = -89.387
-
-    console.log(
-      `https://api.openweathermap.org/data/3.0/onecall/day_summary?lat=${lat}5&lon=${long}&date=${yesterdayString}`,
-    )
-
-    const weather = await fetch(
-      `https://api.openweathermap.org/data/3.0/onecall/day_summary?lat=${lat}5&lon=${long}&date=${yesterdayString}&appid=${weatherApiKey}`,
-      {
-        headers: {
-          accept: 'application/json',
-        },
-      },
-    )
-
-    const weatherResponse = await weather.json()
 
     return new Response(
       JSON.stringify({
         user,
         fields,
-        weather: weatherResponse,
-        weatherCode: weather.status,
+        weather,
       }),
       {
         headers: { ...corsHeaders, 'Content-Type': 'application/json' },
@@ -88,6 +60,7 @@ Deno.serve(async (req: Request) => {
   } catch (error) {
     return new Response(JSON.stringify({ error: error.message }), {
       headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+      error,
       status: 400,
     })
   }
