04a94b18ce1562d70946032f77dfe62c196c56cb
Chris Weed <chris@cjweed.com>
2023-12-10 10:26:14 -0600

    Messages is now a prompt

--- src/components/MessagingPrompt.tsx
+++ src/components/MessagingPrompt.tsx
@@ -1,6 +1,4 @@
-import classNames from 'classnames'
 import { Component, useContext } from 'solid-js'
-import styles from './MessagingPrompt.module.css'
 import { MessagingContext } from './MessagingProvider'
 
 export const MessagingPrompt: Component = () => {
@@ -8,28 +6,11 @@ export const MessagingPrompt: Component = () => {
 
   if (messagingContext?.hasSetupMessaging()) return null
 
-  return (
-    <div
-      class={classNames(styles.MessagingPrompt, {
-        [styles.Hide]: messagingContext?.hasSetupMessaging(),
-      })}
-    >
-      <div class="text-center">Enable notifications?</div>
-      <div class={styles.ActionBlock}>
-        <button
-          class="secondary"
-          type="button"
-          onClick={() => messagingContext?.ignoreMessaging()}
-        >
-          No
-        </button>
-        <button
-          type="button"
-          onClick={() => messagingContext?.setupMessaging()}
-        >
-          Yes
-        </button>
-      </div>
-    </div>
-  )
+  if (window.confirm('Enable notifications?')) {
+    messagingContext?.setupMessaging()
+  } else {
+    messagingContext?.ignoreMessaging()
+  }
+
+  return null
 }
