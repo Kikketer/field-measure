7bb3aa7701e2ffd9206a27efedebecd78921dcd2
Chris Weed <chris@cjweed.com>
2024-03-10 23:28:43 -0500

    And a minor change 3

--- src/components/ReloadPrompt.tsx
+++ src/components/ReloadPrompt.tsx
@@ -2,29 +2,36 @@ import { IonButton } from '@ionic/react'
 import React, { useEffect, useRef } from 'react'
 import './ReloadPrompt.css'
 
+const intervalMS = 60 * 60 * 1000
+
 // @ts-ignore
 import { useRegisterSW } from 'virtual:pwa-register/react'
-import { useVisible } from './VisibleProvider'
 
 function ReloadPrompt() {
-  // const isVisible = useVisible()
-  // const registration = useRef<ServiceWorkerRegistration | null>(null)
-
-  // useEffect(() => {
-  //   if (isVisible && registration.current) {
-  //     console.log('Checking SW', registration.current?.update)
-  //     registration.current?.update?.()
-  //   }
-  // }, [isVisible])
-
   const {
     offlineReady: [offlineReady, setOfflineReady],
     needRefresh: [needRefresh, setNeedRefresh],
     updateServiceWorker,
   } = useRegisterSW({
-    onRegisteredSW(r: ServiceWorkerRegistration) {
-      console.log('SW Registered 2', r)
-      // registration.current = r
+    onRegisteredSW(swUrl: string, r: ServiceWorkerRegistration) {
+      console.log('SW Registered 3', r)
+      // From: https://vite-pwa-org.netlify.app/guide/periodic-sw-updates.html#handling-edge-cases
+      r &&
+        setInterval(async () => {
+          if (!(!r.installing && navigator)) return
+
+          if ('connection' in navigator && !navigator.onLine) return
+
+          const resp = await fetch(swUrl, {
+            cache: 'no-store',
+            headers: {
+              cache: 'no-store',
+              'cache-control': 'no-cache',
+            },
+          })
+
+          if (resp?.status === 200) await r.update()
+        }, intervalMS)
     },
     onRegisterError(error: Error) {
       console.log('SW registration error', error)
